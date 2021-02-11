caa446063608e85f96e03e13e81318af8c891b89
Marcus <danielmarcus05@hotmail.com>
2021-02-11 17:24:11 +0000

    Removing CoreData and implanting Firebase Pt. 2

--- ToDoList/ViewController.swift
+++ ToDoList/ViewController.swift
@@ -19,22 +19,28 @@ class ViewController: UITableViewController {
     // set up coredata
     //Add a sub list functionality...
     
-//    var listItems: [List] = []
+//    var listItems = []
     var subtask = false
     var ref : DatabaseReference!
-    
+    let rootRef = Database.database().reference()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        ref = Database.database().reference()
+        rootRef.child("Lists").observe(DataEventType.value) { (snapshot) in
+            if let listDict = snapshot.value as? [String: AnyObject] {
+                print(listDict)
+                
+                let snap = listDict["item"] as? String
+                
+            }
+        }
         
         title = "To Do List"
         
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
         
         let btnAdd = UIBarButtonItem(title: "Add", style: .plain, target: self, action: #selector(AddItem))
-//        let btnReset = UIBarButtonItem(title: "Reset All", style: .plain, target: self, action: #selector(ResetCheckedItems))
         let btnDeleteAll = UIBarButtonItem(title: "Delete All", style: .plain, target: self, action: #selector(DeleteAllItems))
         
         navigationItem.rightBarButtonItem = btnAdd
@@ -47,7 +53,7 @@ class ViewController: UITableViewController {
         
         guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
         
-        let managedContext = appDelegate.persistentContainer.viewContext
+        
         
     }
     
@@ -55,7 +61,7 @@ class ViewController: UITableViewController {
         let deleteAllAlrt = UIAlertController(title: "Clear List", message: "Sure you want to delete all list items?", preferredStyle: .alert)
         guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
         
-        let managedContext = appDelegate.persistentContainer.viewContext
+        
         
         deleteAllAlrt.addAction(UIAlertAction(title: "Yes", style: .destructive, handler: { (action) in
 //            self.listItems.removeAll()
@@ -79,34 +85,22 @@ class ViewController: UITableViewController {
             self.Save(item: savedItem)
         }))
         
-        addItemAlrt.addAction(UIAlertAction(title: "Add Sub List", style: .default, handler: { (action) in
-            guard let textField = addItemAlrt.textFields?.first, let savedItem = textField.text else { return }
-            self.SaveSubTasks()
-        }))
-        
         addItemAlrt.addTextField(configurationHandler: nil)
         addItemAlrt.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
         self.present(addItemAlrt, animated: true, completion: nil)
     }
     
-    func SaveSubTasks() {
-        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate  else { return }
-        let date = NSDate()
-        let managedContext = appDelegate.persistentContainer.viewContext
-        
-//        guard let entity = NSEntityDescription.entity(forEntityName: "", in: <#T##NSManagedObjectContext#>)
-    }
     
     func Save(item: String) {
         guard let appDelegate = UIApplication.shared.delegate as? AppDelegate  else { return }
-        let date = NSDate()
-        let managedContext = appDelegate.persistentContainer.viewContext
+        let dateFormat = DateFormatter()
+        dateFormat.dateFormat = "dd-MM-yy ss:mm:hh"
+        let date = dateFormat.string(from: Date())
         
-//        if booleanState == true {
-//            itm.setValue(subtask = true, forKey: "subTasks")
-//        }
+        let data = ["item": item, "date": date, "hasSubList": false] as [String : Any]
+        let ref = rootRef.child("Lists").childByAutoId()
         
-        
+        ref.setValue(data)
     }
     
     
@@ -143,7 +137,7 @@ class ViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
         let deleteAction = UIContextualAction(style: .destructive, title: "Delete") { (action, view, handler) in
              guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
-            let managedContext = appDelegate.persistentContainer.viewContext
+            
 //            managedContext.delete(self.listItems[indexPath.row])
             handler(true)
         }
